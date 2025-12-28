b36218e884d71b1a089c9c2f6821777b42edf7e3
Fady Malak <fady__malak@outlook.com>
2025-12-28 05:41:56 +0200

    api doc6

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// API Documentation (Scribe)
+// After installing Scribe, this will serve the docs at /api/doc
+// Install: composer require knuckleswtf/scribe
+// Generate: php artisan scribe:generate
